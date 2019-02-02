41c48b974b146fddd0d49285fda750edc79def4d
Randy <45516191+jamazn@users.noreply.github.com>
2019-02-02 02:22:44 -0500

    Add files via upload

--- Scrubber.py
+++ Scrubber.py
@@ -1,16 +1,19 @@
 import re                           # Import the regex module.
+import pickle
 
 err_occur = []                         # The list where we will store results.
+new_list = []
 pattern = re.compile(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b") # Compile regular expression to match any phone number.
 try:                              # Try to:
-    with open ('ip_page.txt', 'rt') as in_file:          # open file for reading text.
+    with open ('ip_page.txt', 'rt', encoding='windows-1252') as in_file:          # open file for reading text.
         for linenum, line in enumerate(in_file):        # Keep track of line numbers.
-            if pattern.search(line) != None:          # If pattern search finds a match,
+              if pattern.search(line) != None:          # If pattern search finds a match,
+                err_occur.append((linenum, line.rstrip('\n'))) # strip linebreaks, store line and line number as tuple.
+                start = '/lookup/'
+                end = "'>"
                 try:
-                    err_occur.append((linenum, line.rstrip('\n'))) # strip linebreaks, store line and line number as tuple.
-                    print("Line ", linenum, ": ", line, sep='')  # print results as "Line [linenum]: [line]".
-                except Error:
-                    print("Error")               # print an error message.
-                    exit()
+                    new_list.append(line.split(start))[1].split(end)[0])
+                except:
+                    print("Error")
 except FileNotFoundError:                   # If input file not found,
     print("Input file not found.")               # print an error message. 
